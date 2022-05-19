bd11ad44eca139b3ff0c64993c6fc76dcd7d2b23
robertolatini31 <robertolatini31@gmail.com>
2022-05-19 14:28:47 +0200

    add bad words censorship

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+$text = 'PHP (acronimo ricorsivo di PHP: Hypertext Preprocessor, preprocessore di ipertesti; originariamente acronimo di (Personal Home Page) è un linguaggio di scripting interpretato, originariamente concepito per la programmazione di pagine web dinamiche. L\'interprete PHP è un software libero distribuito sotto la PHP License.';
+echo "<pre>";
+var_dump($text);
+"</pre>";
+
+$censored_text = str_replace($_GET['bad_word'], '***', $text);
+echo "<pre>";
+var_dump($censored_text);
+"</pre>";
 
 ?>
 
